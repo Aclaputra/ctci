f2b0f2b659bd8fa75e95aa583cf8ffbe41eadc51
Aclaputra <muhamadacla22@gmail.com>
2022-11-25 12:53:37 -0600

    feat: adding isuniquechars java

---
+++ src/main/java/ctci/IsUniqueChars.java
@@ -0,0 +1,24 @@
+package ctci;
+
+public class IsUniqueChars {
+  static boolean validateUniqueChars(String str) {
+    if (str.length() > 128) return false;
+
+    boolean[] char_set = new boolean[128];
+    for (int i = 0; i < str.length(); i++) {
+      int val = str.charAt(i);
+      if (char_set[val]) { // Already found this char in string
+        return false;
+      }
+      char_set[val] = true;
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    // false
+    System.out.println(validateUniqueChars("nani"));
+    // true
+    System.out.println(validateUniqueChars("nice"));
+  }
+}
