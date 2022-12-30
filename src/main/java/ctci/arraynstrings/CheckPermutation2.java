3384e7544f5571a9787aba6786c0dc07971495e2
Aclaputra <muhamadacla22@gmail.com>
2022-12-31 04:45:48 +0700

    feat: added checkpermutation

---
+++ src/main/java/ctci/arraynstrings/CheckPermutation2.java
@@ -0,0 +1,20 @@
+package ctci.arraynstrings;
+
+import java.io.PrintStream;
+
+public class CheckPermutation2 {
+    static PrintStream out = System.out;
+    public static boolean permutation(String s, String t) {
+        /**
+         * 1. if both arguments doesn't have same length then false.
+         * 2. declare variable store index string into char array.
+         * 3. declare variable store letters int array indexing how much per word appear.
+         */
+        return true;
+    }
+    public static void main(String[] args) {
+        out.println(permutation("fis", "dog"));
+        out.println(permutation("fsih", "fish"));
+        out.println(permutation("king", "queen"));
+    }
+}
